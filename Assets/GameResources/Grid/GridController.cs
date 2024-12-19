e6266ba739d48b15fbbfcd09d36245af3e81fc44
Napwob <gorno-altaisk0@mail.ru>
2024-12-19 15:40:37 +0300

    Fix grid system

--- Assets/GameResources/Grid/GridController.cs
+++ Assets/GameResources/Grid/GridController.cs
@@ -3,48 +3,75 @@ using System.Collections.Generic;
 
 public class GridController : MonoBehaviour
 {
+    [SerializeField] private Transform gridParent;
     [SerializeField] private GameObjectPool cellPool;
-    [SerializeField, Range(1, 20)] private int cellSpacing = 10;
+    [SerializeField] private float cellSpacing = 10f;
+    private AnimationHandler animationHandler;
 
-    public void GenerateGrid(LevelData levelData, System.Action<CellController> onCellClicked)
+    private void Awake()
+    {
+        animationHandler = GetComponent<AnimationHandler>();
+    }
+
+    public List<Sprite> GenerateGrid(LevelData levelData, System.Action<CellController> onCellClicked)
     {
         ClearGrid();
 
+        List<Sprite> gridSprites = new List<Sprite>();
+        List<Sprite> availableSprites = GetAllAvailableSprites(levelData);
         float cellWidth = cellPool.GetPooledObject().GetComponent<RectTransform>().sizeDelta.x;
         float cellHeight = cellPool.GetPooledObject().GetComponent<RectTransform>().sizeDelta.y;
+
+        float indentRow = (levelData.Rows - 1) * (cellWidth + cellSpacing);
+        float indentCol = (levelData.Columns - 1) * (cellHeight + cellSpacing);
 
         for (int row = 0; row < levelData.Rows; row++)
         {
             for (int column = 0; column < levelData.Columns; column++)
             {
+                if (availableSprites.Count == 0)
+                {
+                    Debug.LogError("Not enough unique sprites to fill the grid!");
+                    break;
+                }
+
                 GameObject cell = cellPool.GetPooledObject();
-                cell.transform.SetParent(gameObject.transform, false);
+                cell.transform.SetParent(gridParent, false);
                 cell.SetActive(true);
 
                 RectTransform rectTransform = cell.GetComponent<RectTransform>();
                 rectTransform.anchoredPosition = new Vector2(
-                    column * (cellWidth + cellSpacing),
-                    -row * (cellHeight + cellSpacing)
+                    (-indentCol)/2 + column * (cellWidth + cellSpacing),
+                    indentRow/2 + (- row) * (cellHeight + cellSpacing)
                 );
 
-                Sprite sprite = GetSpriteByIndex(levelData, row * levelData.Columns + column);
+                animationHandler.BounceIn(rectTransform);
+
+                Sprite sprite = availableSprites[Random.Range(0, availableSprites.Count)];
+                availableSprites.Remove(sprite);
+                gridSprites.Add(sprite);
+
                 CellController cellController = cell.GetComponent<CellController>();
                 cellController.Setup(sprite, onCellClicked);
             }
         }
+
+        return gridSprites;
     }
 
-    private Sprite GetSpriteByIndex(LevelData levelData, int index)
+    private List<Sprite> GetAllAvailableSprites(LevelData levelData)
     {
-        int groupIndex = index % levelData.AllowedSpriteGroups.Count;
-        SpriteGroup selectedGroup = levelData.AllowedSpriteGroups[groupIndex];
-        int spriteIndex = index % selectedGroup.Sprites.Count;
-        return selectedGroup.Sprites[spriteIndex];
+        List<Sprite> allSprites = new List<Sprite>();
+        foreach (var group in levelData.AllowedSpriteGroups)
+        {
+            allSprites.AddRange(group.Sprites);
+        }
+        return allSprites;
     }
 
     private void ClearGrid()
     {
-        foreach (Transform child in gameObject.transform)
+        foreach (Transform child in gridParent)
         {
             child.gameObject.SetActive(false);
         }
